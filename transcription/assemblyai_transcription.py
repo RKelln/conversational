24f45f1c8181d372ea7d5394fe29e1cf9ff89e62
Ryan Kelln <ryan.kelln@gmail.com>
2024-04-25 16:25:21 -0400

    assembly ai code cleanup

--- transcription/assemblyai_transcription.py
+++ transcription/assemblyai_transcription.py
@@ -15,58 +15,14 @@ from dotenv import load_dotenv
 load_dotenv()
 aai.settings.api_key = os.getenv('ASSEMBLYAI_API_KEY')
 
-"""
-def on_open(session_opened: aai.RealtimeSessionOpened):
-    "This function is called when the connection has been established."
-
-    print("Session ID:", session_opened.session_id)
-
-def on_data(transcript: aai.RealtimeTranscript):
-    "This function is called when a new transcript has been received."
-
-    if not transcript.text:
-        return
-
-    if isinstance(transcript, aai.RealtimeFinalTranscript):
-        print(transcript.text, end="\r\n")
-    else:
-        print(transcript.text, end="\r")
-
-def on_error(error: aai.RealtimeError):
-    "This function is called when the connection has been closed."
-
-    print("An error occured:", error)
-
-def on_close():
-    "This function is called when the connection has been closed."
-
-    print("Closing Session")
-
-transcriber = aai.RealtimeTranscriber(
-    on_data=on_data,
-    on_error=on_error,
-    sample_rate=44_100,
-    on_open=on_open, # optional
-    on_close=on_close, # optional
-)
-
-# Start the connection
-transcriber.connect()
-
-# Open a microphone stream
-print("open mic")
-microphone_stream = aai.extras.MicrophoneStream()
-
-# Press CTRL+C to abort
-print("Press CTRL+C to stop the transcription")
-transcriber.stream(microphone_stream)
-
-transcriber.close()
-
-"""
-
 
 class AssemblyAITranscription(TranscriptionService):
+
+    valid_options = [
+        # Assembly ai options: https://www.assemblyai.com/docs/speech-to-text/streaming
+        "sample_rate", "end_utterance_silence_threshold", "word_boost", "encoding", "disable_partial_transcripts"
+    ]
+
     def __init__(self, 
                  transcript_callback=None,
                  callbacks: dict = {"on_message": True}, 
@@ -78,10 +34,16 @@ class AssemblyAITranscription(TranscriptionService):
         self.current_transcript = "" # for the current utterance
         self.full_transcript = "" # all utterances
 
+        # extract acceptable options, keep only the keys that are valid
+        if options is not None:
+            print("Options: ", options)
+            options = {k: v for k, v in options.items() if k in AssemblyAITranscription.valid_options}
+
         # options: https://www.assemblyai.com/docs/speech-to-text/streaming
         self.options = dict(    
             sample_rate=44_100,
             end_utterance_silence_threshold=500,
+            disable_partial_transcripts=True,
         )
         if options is not None:
             self.options.update(options)
